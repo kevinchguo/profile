9ce01feec551d6d102b65e1665af5e79267ad9b9
Kevinnnface <45556758+Kevinnnface@users.noreply.github.com>
2020-10-27 23:28:59 -1000

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Route, Switch, Link } from "react-router-dom";
 import "./App.css";
+import Home from "./views/Home"
+import About from "./views/About"
+import Projects from "./views/Projects"
+import Contact from "./views/Home"
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello, My name is Kevin and this is my page.</h1>
+      <h1>Kevin Guo</h1>
+      <div className="navbar">
+        <button><Link to="/home">Home</Link></button>
+        <button><Link to="/about">About</Link></button>
+        <button><Link to="/projects">Projects</Link></button>
+        <button><Link to="/contact">Contact</Link></button>
+
+      </div>
+        <Switch>
+          <Route path="/home" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/projects" component={Projects}/>
+          <Route path="/contact" component={Contact}/>
+        </Switch>
     </div>
   );
 }
